78ee633f3704a38d84be77a73cfa33b6af449926
Prince <reberoprince@gmail.com>
2021-09-25 22:03:42 -0400

    minor change

--- scan.cpp
+++ scan.cpp
@@ -20,7 +20,7 @@ token scan()
     /* next available char; extra (int) width accommodates EOF */
     int i = 0; /* index into token_image */
 
-    /* skip white space */
+    // /* skip white space */
     // while (isspace(c))
     // {
     //     c = getchar();
@@ -45,6 +45,18 @@ token scan()
             return t_read;
         else if (!strcmp(token_image, "write"))
             return t_write;
+          else if (!strcmp(token_image, "if"))
+            return t_if;
+              else if (!strcmp(token_image, "fi"))
+            return t_fi;
+         else if (!strcmp(token_image, "check"))
+            return t_check;
+         else if (!strcmp(token_image, "do"))
+            return t_do;
+         else if (!strcmp(token_image, "od"))
+            return t_od;
+         else if (!strcmp(token_image, "eof"))
+            return t_eof;
         else
             return t_id;
     }
@@ -77,6 +89,114 @@ token scan()
                 return t_gets;
             }
             break;
+        case '=':
+            std::cin.get(c);
+
+
+            if (c == '=')
+            {
+                std::cin.get(c);
+                return t_dequals;
+            }
+            
+            else
+            {
+               fprintf(stderr,
+                        "expected '=' after '=', got '%c' (0x%x)\n",
+                        c, c);
+                exit(1);
+            }
+            break;
+  case '$':
+            std::cin.get(c);
+
+
+            if (c == '$')
+            {
+                std::cin.get(c);
+                return t_eof;
+            }
+         case '>':
+            std::cin.get(c);
+
+
+            if (c == '=')
+            {
+                std::cin.get(c);
+                return t_ge;
+            }
+
+            
+  if(c == ' ')
+            {
+               
+                return t_gt;
+            }
+            
+            else
+            {
+               fprintf(stderr,
+                        "expected '=' after '=', got '%c' (0x%x)\n",
+                        c, c);
+                exit(1);
+            }
+            break;
+ case '<':
+            std::cin.get(c);
+
+
+            if (c == '=')
+            {
+                std::cin.get(c);
+                return t_ge;
+            }
+
+            
+  if(c == ' ')
+            {
+               
+                return t_lt;
+            }
+              if(c == '>')
+            {
+               std::cin.get(c);
+                return t_nequal;
+            }
+
+             if(c == '=')
+            {
+               std::cin.get(c);
+                return t_le;
+            }
+            
+            else
+            {
+               fprintf(stderr,
+                        "expected '=' after '=', got '%c' (0x%x)\n",
+                        c, c);
+                exit(1);
+            }
+            break;
+
+            //  case '<':
+            // std::cin.get(c);
+
+
+            // if (c == '=')
+            // {
+            //     std::cin.get(c);
+            //     return t_gets;
+            // }
+
+            // if (c == ' ')
+            // {
+            //     std::cin.get(c);
+            //     return t_le;
+            // }
+            
+
+
+        
         case '+':
                 std::cin.get(c);
             return t_add;
@@ -89,12 +209,17 @@ token scan()
         case '/':
                 std::cin.get(c);
             return t_div;
+         
+                return t_eof;
+           
+            return t_add;
         case '(':
                 std::cin.get(c);
             return t_lparen;
         case ')':
                 std::cin.get(c);
             return t_rparen;
+        
         
         default:
             fprintf(stderr, "unexpected character '%c' (0x%x)\n", c, c);
